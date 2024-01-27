a98f3a0e44be72555b09e66ee0a361cd7f6092a6
Soren <sor3nnw@gmail.com>
2024-01-27 17:06:51 -0600

    Co-authored-by: Nathan Zuniga <NathanZunigaCS@users.noreply.github.com> Co-authored-by: Jibril Ahmed Pascua (student) <JibrilAhmed.Pascua@my.utsa.edu>

--- App.js
+++ App.js
@@ -1,119 +1,70 @@
-import * as React from 'react'; 
+import React from 'react';
 import {StyleSheet ,Button, View, Text, TextInput , FlatList,TouchableOpacity,Image,Modal} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { CommonActions, NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { BottomNavigation , PaperProvider, DefaultTheme} from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import { PaperProvider } from 'react-native-paper';
 import {lightColors} from './lightMode.json';
 import {darkColors} from './darkMode.json';
 import {useState} from 'react';
-import { CommonActions } from '@react-navigation/native';
-import {BottomNavigation } from 'react-native-paper';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {images} from './Constants.json';
 
-
-
-
-function HomeScreen({ navigation}) {
-  const lightTheme = {lightColors}
-  const darkTheme = {darkColors}
+const customTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: lightColors.primary,
+    accent: "black",
+    background: lightColors.background,
+    
+  },
+}
+function HomeScreen() {
+  const lightTheme = {lightColors};
+  const [budget, setBudget] = useState('');
   return (
-    <SafeAreaProvider>
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' , backgroundColor: lightTheme.lightColors.background}}>
-        <Text>Home Screen</Text>
-        <Button 
-          title="Go to Details"
-          onPress={() => {
-            /* 1. Navigate to the Details route with params */
-            navigation.navigate('Details', {
-              itemId: 1,
-              otherParam: 'anything you want here',
-            });
-          }}
-        />
-        <Button
-          title="Go to About"
-          onPress={() => {
-            /* 1. Navigate to the Details route with params */
-            navigation.navigate('About', {
-              itemId: 2,
-              otherParam: 'anything you want here',
-            });
-          }}
-        />
-        <Button title="Nathan's Practicing Here, Will delete later"
-        onPress={() => {
-          navigation.navigate('NathanPractice', { 
-            itemId: 3,
-            otherParam: 'anything you want here'
-          });
-        }}
-        >
-          
-        </Button>
-        
-      </View>
-      
-    </SafeAreaProvider>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center' ,backgroundColor: lightTheme.lightColors.background}}>
+      <Text variant="headlineMedium">Home!</Text>
+      <TextInput label="budget" value={budget} onChangeText={setBudget} />
+
+    </View>
   );
 }
 
-function DetailsScreen() {
+function SettingsScreen() {
   return (
-    <SafeAreaProvider>
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-      </View>
-    </SafeAreaProvider>
-  );
+    <View style={styles.container}>
+      <Text variant="headlineMedium">Settings!</Text>
+    </View>
+  );  
 }
-
 function AboutScreen() {
   return (
       <SafeAreaProvider>
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text style={{}}> Here is the about screen for Credit Confidence! </Text>
+          <Text style={{}}> Here is the about screen for Credit Confidence!  </Text>
+          <Image source={require('./assets/Turt.png')}></Image>
+
         </View>
       </SafeAreaProvider>
   );
 }
 
 
-
-function NathanPractice() {
+function Goals() {
   const [text, setText] = useState('');
-  const [count, setCount] = useState(0);
+  
   return (
     <SafeAreaProvider>
       
-<View>
-  <Text> {count} </Text>
-  <Button onPress={ () => setCount(count + 1)
-    
-  }></Button>
-</View>
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text> Ello </Text>
+      </View>
     </SafeAreaProvider>
   );
 }
-
-const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
-function App() {
-  return (
-    <PaperProvider>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Welcome' }}/>
-          <Stack.Screen name="Details" component={DetailsScreen} />
-          <Stack.Screen name="About" component={AboutScreen} />
-          <Stack.Screen name="NathanPractice" component={NathanPractice} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </PaperProvider>
-  );
-}
-
 
 const styles = StyleSheet.create({
   container: {
@@ -124,4 +75,106 @@ const styles = StyleSheet.create({
 });
 
 
-export default App;
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  const lightTheme = {lightColors};
+  return (
+    <PaperProvider theme={customTheme}>
+    <NavigationContainer >
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+      tabBar={({ navigation, state, descriptors, insets }) => (
+        <BottomNavigation.Bar
+          
+          barStyle={{ backgroundColor: lightTheme.lightColors.primary }}
+          activeColor={lightTheme.lightColors.primary}
+          inactiveColor={lightTheme.lightColors.primary}
+          navigationState={state}
+         safeAreaInsets={insets}
+          onTabPress={({ route, preventDefault }) => {
+            const event = navigation.emit({
+              type: 'tabPress',
+              target: route.key,
+              canPreventDefault: true,
+            });
+
+            if (event.defaultPrevented) {
+              preventDefault();
+            } else {
+             navigation.dispatch({
+                ...CommonActions.navigate(route.name, route.params),
+                target: state.key,
+              });
+            }
+          }}
+          renderIcon={({ route, focused, color }) => {
+            const { options } = descriptors[route.key];
+            if (options.tabBarIcon) {
+              return options.tabBarIcon({ focused, color, size: 24 });
+            }
+
+            return null;
+          }}
+          getLabelText={({ route }) => {
+            const { options } = descriptors[route.key];
+            const label =
+              options.tabBarLabel !== undefined
+                ? options.tabBarLabel
+                : options.title !== undefined
+                ? options.title
+                : route.title;
+
+            return label;
+          }}
+        />
+      )}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => {
+            return <Icon name="home" size={size} color={lightTheme.lightColors.primary} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Goals"
+        component={Goals}
+        options={{
+          tabBarLabel: 'Goals',
+          tabBarIcon: ({ color, size }) => {
+            return <Icon name="cash" size={size} color={lightTheme.lightColors.primary} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="About"
+        component={AboutScreen}
+        options={{
+          tabBarLabel: 'About',
+          tabBarIcon: ({ color, size }) => {
+            return <Icon name="information-outline" size={size} color={lightTheme.lightColors.primary} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          tabBarLabel: 'Settings',
+          tabBarIcon: ({ color, size }) => {
+            return <Icon name="cog" size={size} color={lightTheme.lightColors.primary} />;
+          },
+        }}
+      />
+    </Tab.Navigator>
+    </NavigationContainer>
+    </PaperProvider>
+  );
+}
+
