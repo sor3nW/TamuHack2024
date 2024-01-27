0185943c74d02a2beb1c3996e9ca7a89473dbad1
Soren <sor3nnw@gmail.com>
2024-01-27 15:42:47 -0600

    Co-authored-by: Nathan Zuniga <NathanZunigaCS@users.noreply.github.com> Co-authored-by: Jibril Ahmed Pascua (student) <JibrilAhmed.Pascua@my.utsa.edu>

--- App.js
+++ App.js
@@ -1,20 +1,127 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react'; 
+import {StyleSheet ,Button, View, Text, TextInput , FlatList,TouchableOpacity,Image,Modal} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import { PaperProvider } from 'react-native-paper';
+import {lightColors} from './lightMode.json';
+import {darkColors} from './darkMode.json';
+import {useState} from 'react';
+import { CommonActions } from '@react-navigation/native';
+import {BottomNavigation } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
+
+
+
+function HomeScreen({ navigation}) {
+  const lightTheme = {lightColors}
+  const darkTheme = {darkColors}
   return (
-    <View style={styles.container}>
-      <Text>Josh will help me develop</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' , backgroundColor: lightTheme.lightColors.background}}>
+        <Text>Home Screen</Text>
+        <Button 
+          title="Go to Details"
+          onPress={() => {
+            /* 1. Navigate to the Details route with params */
+            navigation.navigate('Details', {
+              itemId: 1,
+              otherParam: 'anything you want here',
+            });
+          }}
+        />
+        <Button
+          title="Go to About"
+          onPress={() => {
+            /* 1. Navigate to the Details route with params */
+            navigation.navigate('About', {
+              itemId: 2,
+              otherParam: 'anything you want here',
+            });
+          }}
+        />
+        <Button title="Nathan's Practicing Here, Will delete later"
+        onPress={() => {
+          navigation.navigate('NathanPractice', { 
+            itemId: 3,
+            otherParam: 'anything you want here'
+          });
+        }}
+        >
+          
+        </Button>
+        
+      </View>
+      
+    </SafeAreaProvider>
   );
 }
+
+function DetailsScreen() {
+  return (
+    <SafeAreaProvider>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    </SafeAreaProvider>
+  );
+}
+
+function AboutScreen() {
+  return (
+      <SafeAreaProvider>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text style={{}}> Here is the about screen for Credit Confidence! </Text>
+        </View>
+      </SafeAreaProvider>
+  );
+}
+
+
+
+function NathanPractice() {
+  const [text, setText] = useState('');
+  const [count, setCount] = useState(0);
+  return (
+    <SafeAreaProvider>
+      
+<View>
+  <Text> {count} </Text>
+  <Button onPress={ () => setCount(count + 1)
+    
+  }></Button>
+</View>
+    </SafeAreaProvider>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+function App() {
+  return (
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Welcome' }}/>
+          <Stack.Screen name="Details" component={DetailsScreen} />
+          <Stack.Screen name="About" component={AboutScreen} />
+          <Stack.Screen name="NathanPractice" component={NathanPractice} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  );
+}
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
+
+
+export default App;
